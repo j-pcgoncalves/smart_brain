03ca8db4795e8e7c22b8fd79093eee34e779608e
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-10-22 14:14:56 +0100

    create and style ImageLinkForm component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from "./components/Navigation/Navigation";
 import SignIn from "./components/SignIn/SignIn";
 import Register from "./components/Register/Register";
 import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import './App.css';
 
@@ -28,7 +29,7 @@ class App extends Component {
     this.state = initialState;
   }
 
-  loadUser = (data) => {
+  loadUser = data => {
     this.setState({user: {
       id: data.id,
       name: data.name,
@@ -38,7 +39,56 @@ class App extends Component {
     }})
   }
 
+  calculateFaceLocation = data => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("input_image");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = box => {
+    this.setState({box: box});
+  }
   
+  onInputChange = e => {
+    this.setState({input: e.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    fetch("http://localhost:3000/imageurl", {
+      method: "post",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(res => res.json())
+    .then(res => {
+      if (res) {
+        fetch("http://localhost:3000/image", {
+          method: "put",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(res => res.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, { entries: count }))
+        })
+        .catch(console.log)
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(e => console.log(e));
+  }
 
   onRouteChange = route => {
     if (route === "signout") {
@@ -61,6 +111,10 @@ class App extends Component {
               <Rank 
                 name={this.state.user.name}
                 entries={this.state.user.entries}
+              />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
               />
             </div>
           : (
