c734113e13e856e6e8d65afac6c64db97b64a7c4
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-03-26 19:59:11 +0000

    add dropdown functionality to ProfileIcon

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,15 +1,40 @@
 import { useState } from "react";
+import { 
+    Dropdown, 
+    DropdownToggle, 
+    DropdownMenu, 
+    DropdownItem 
+} from "reactstrap";
 
 const ProfileIcon = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
+    const toggle = () => {
+        setDropdownOpen(!dropdownOpen);
+    }
+
     return (
         <div className="pa4 tc">
-            <img 
-                src="http://tachyons.io/img/logo.jpg" 
-                className="br-100 ba h3 w3 dib" 
-                alt="Avatar" 
-            />
+            <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                <DropdownToggle
+                    tag="span"
+                    data-toggle="dropdown"
+                    aria-expanded={dropdownOpen}
+                >
+                    <img 
+                        src="https://avatar.iran.liara.run/public/42g" 
+                        className="br-100 ba h3 w3 dib" 
+                        alt="Avatar" 
+                    />
+                </DropdownToggle>
+                <DropdownMenu 
+                    className="b--transparent shadow-5" 
+                    style={{ marginTop: "20px", backgroundColor: "rgba(255, 255, 255, 0.5)" }}
+                >
+                    <DropdownItem>View Profile</DropdownItem>
+                    <DropdownItem>Sign Out</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
         </div>
     );
 }
