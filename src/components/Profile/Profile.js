76d0532f2b864fba346ad516e4fb1bbbe73b5134
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-03-28 19:20:38 +0000

    create Profile form state

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,28 @@
+import { useState } from "react";
+
 import "./Profile.css";
 
 const Profile = ({ toggleModal, user }) => {
+    const [name, setName] = useState(user.name);
+    const [age, setAge] = useState(user.age);
+    const [pet, setPet] = useState(user.pet);
+
+    const onFormChange = (event) => {
+        switch(event.target.name) {
+            case "user-name":
+                setName(event.target.value);
+                break;
+            case "user-age":
+                setAge(event.target.value);
+                break;
+            case "user-pet":
+                setPet(event.target.value);
+                break;
+            default:
+                return;
+        }
+    }
+
     return (
         <div className="profile-modal">
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
@@ -10,33 +32,36 @@ const Profile = ({ toggleModal, user }) => {
                         className="h3 w3 dib" 
                         alt="Avatar" 
                     />
-                    <h1>{user.name}</h1>
+                    <h1>{name}</h1>
                     <h4>{`Images Submitted: ${user.entries}`}</h4>
                     <p>{`Member since: ${new Date(user.joined).toLocaleDateString()}`}</p>
                     <hr />
                     <label className="mt2 fw6" htmlFor="user-name">Name:</label>
                     <input 
                         className="pa2 ba w-100"
-                        placeholder={user.name}
+                        placeholder={name}
                         type="text"
                         name="user-name"
                         id="name"
+                        onChange={onFormChange}
                     />
                     <label className="mt2 fw6" htmlFor="user-age">Age:</label>
                     <input 
                         className="pa2 ba w-100"
-                        placeholder={user.age}
+                        placeholder={age}
                         type="text"
                         name="user-age"
                         id="age"
+                        onChange={onFormChange}
                     />
                     <label className="mt2 fw6" htmlFor="user-pet">Pet:</label>
                     <input 
                         className="pa2 ba w-100"
-                        placeholder={user.pet}
+                        placeholder={pet}
                         type="text"
                         name="user-pet"
                         id="pet"
+                        onChange={onFormChange}
                     />
                     <div className="mt4" style={{ display: "flex", justifyContent: "space-evenly" }}>
                         <button className="b pa2 grow pointer hover-white w-40 bg-light-blue b--black-20">
