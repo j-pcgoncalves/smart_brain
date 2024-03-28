a34bbfb550a55fcaf127f2102f7b27bd697589b1
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-03-28 21:30:03 +0000

    solve bug with data properties

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -29,9 +29,9 @@ class SignIn extends React.Component {
             })
         })
             .then(res => res.json())
-            .then(user => {
-                if (user.id) {
-                    this.props.loadUser(user);
+            .then(data => {
+                if (data.userId) {
+                    this.props.loadUser(data);
                     this.props.onRouteChange("home");
                 }
             })
