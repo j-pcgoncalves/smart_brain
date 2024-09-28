3687940f5427fd153431ee8208f4f0960758b8b8
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-09-28 13:16:35 +0100

    connect with clarifai API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
+import { useState } from 'react';
+
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import { SECRET_CONFIG } from './secretConfig';
 
 import ParticlesBg from 'particles-bg';
 
 import './App.css';
 
 const App = () => {
+  const [input, setInput] = useState("");
+
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = SECRET_CONFIG.CLARIFAI_PAT_KEY;
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = "clarifai";       
+  const APP_ID = "main";
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';   
+  const IMAGE_URL = input;
+
+  const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Key ' + PAT,
+      },
+      body: raw
+  };
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  }
+
+  const onButtonSubmit = () => {
+    fetch("https://cors-anywhere.herokuapp.com/https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+  }
+
   return (
     <div className="App">
       <ParticlesBg 
@@ -20,7 +71,10 @@ const App = () => {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm 
+        onInputChange={onInputChange} 
+        onButtonSubmit={onButtonSubmit} 
+      />
       {/* <FaceRecognition /> */}
     </div>
   );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,16 +2,26 @@ import React from "react";
 
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className="f3">
                 {"This Magic Brain will detect faces in your pictures. Give it a try!"}
             </p>
             <div className="center">
-                <div style={{ backdropFilter: "blur(5px)" }} className="form center pa4 br3 shadow-5">
-                    <input className="f4 pa2 w-70 center" type="text" />
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-blue ba">Detect</button>
+                <div 
+                    style={{ backdropFilter: "blur(5px)" }} 
+                    className="form center pa4 br3 shadow-5"
+                >
+                    <input 
+                        className="f4 pa2 w-70 center" 
+                        type="text" 
+                        onChange={onInputChange} 
+                    />
+                    <button 
+                        className="w-30 grow f4 link ph3 pv2 dib white bg-blue ba"
+                        onClick={onButtonSubmit}
+                    >Detect</button>
                 </div>
             </div>
         </div>
